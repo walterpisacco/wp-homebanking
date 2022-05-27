a504a7ba03ded9f90176932e3357f519021e60cf
walterpisacco <65618158+walterpisacco@users.noreply.github.com>
2022-05-27 09:18:51 -0300

    Update conectar_sqlsrv.php

--- Modulos/Utilidades/conectar_sqlsrv.php
+++ Modulos/Utilidades/conectar_sqlsrv.php
@@ -9,10 +9,7 @@ class conectarBase{
 	 * @return Object
 	 */
 	public function ConsultaSelect($query){
-		//$conn = mysqli_connect("iothinks.drexgen.com","admin_iothinks","Global*3522","seguridad");
-		$conn = mysqli_connect("aktis.drexgen.com","admin_iothinks","Global*3522","seguridad");
-		//$conn = mysqli_connect("lg.drexgen.com","admin_iothinks","Global*3522","seguridad");
-		//$conn = mysqli_connect("localhost","admin_iothinks","Global*3522","seguridad");
+		$conn = mysqli_connect("localhost","","","");
 	    if ($execquery = $conn->query($query)) {
 	        $result = $execquery->fetch_all(MYSQLI_ASSOC);
 	        return $result;
